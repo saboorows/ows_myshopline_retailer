ce7a0d81e2cc806abd820d29f49180835348d216
smsaboor <48467571+smsaboor@users.noreply.github.com>
2022-10-24 23:37:29 +0530

    Initial Commit

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const appTitle = "MyShopline Sellers";
+  static const loginTitle = "Login";
+  static const homeTitle = "Home";
+  static const settingsTitle = "Settings";
+  static const fullPhotoTitle = "Full Screen";
+}
